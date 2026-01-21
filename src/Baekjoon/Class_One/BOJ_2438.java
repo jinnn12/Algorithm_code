0f678c25b9744b2d25864cda1916b66d11065aad
prohh <prohh12@naver.com>
2026-01-21 22:26:36 +0900

    BOJ_2438

---
+++ src/Baekjoon/Class_One/BOJ_2438.java
@@ -0,0 +1,25 @@
+package Baekjoon.Class_One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
