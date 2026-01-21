3033b5f139f9844705baec937e68cd61d2e96457
prohh <prohh12@naver.com>
2026-01-21 19:55:12 +0900

    BOJ_1001

---
+++ src/Baekjoon/Class_One/BOJ_1001.java
@@ -0,0 +1,20 @@
+package Baekjoon.Class_One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = br.readLine().split(" ");
+
+        int A = Integer.parseInt(inputs[0]);
+        int B = Integer.parseInt(inputs[1]);
+
+        System.out.println(A - B);
+
+        br.close();
+    }
+}
