1c697618cce2439ab491235a43c1400ce2162554
prohh <prohh12@naver.com>
2026-01-21 23:11:33 +0900

    BOJ_2741

---
+++ src/Baekjoon/Class_One/BOJ_2741.java
@@ -0,0 +1,17 @@
+package Baekjoon.Class_One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2741 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
