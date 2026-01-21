baca0e5acef19952aa7747ef64a2309b437017fb
prohh <prohh12@naver.com>
2026-01-21 23:43:29 +0900

    BOJ_10871
    - 첫트 실패, 보자마자 생각 안 남
    - 오늘은 자고 내일 아침에 다시해보기!

---
+++ src/Baekjoon/Class_One/BOJ_10871.java
@@ -0,0 +1,14 @@
+package Baekjoon.Class_One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+    }
+}
