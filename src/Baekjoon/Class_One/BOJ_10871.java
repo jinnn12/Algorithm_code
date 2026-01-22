14329ce0e0878b5afae21a50dc15693bce6dc1bb
jinnn12 <prohh12@naver.com>
2026-01-22 14:49:46 +0900

    BOJ_10871

--- src/Baekjoon/Class_One/BOJ_10871.java
+++ src/Baekjoon/Class_One/BOJ_10871.java
@@ -8,6 +8,25 @@ public class BOJ_10871 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String[] inputs = br.readLine().split(" ");
+
+        int N = Integer.parseInt(inputs[0]);
+        int X = Integer.parseInt(inputs[1]);
+
+        String[] sequence = br.readLine().split(" ");
+        int[] arr = new int[sequence.length];
+
+        for (int i = 0; i < N; i++) {
+            int y = Integer.parseInt(sequence[i]);
+            if (y < X) {
+                System.out.print(y + " ");
+            }
+
+        }
+
+
+
+
 
 
     }
