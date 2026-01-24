d02a6410238c41fdba9794a995f4041c661453e3
jinnn12 <prohh12@naver.com>
2026-01-24 17:44:57 +0900

    BOJ_31403

---
+++ src/Baekjoon/Class_One/BOJ_31403.java
@@ -0,0 +1,20 @@
+package Baekjoon.Class_One;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_31403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
+
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c));
+        System.out.println(Integer.parseInt(a + b) - Integer.parseInt(c));
+
+
+    }
+}
